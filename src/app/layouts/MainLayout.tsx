040337f99a30d72f25eb722aa40ea1942847e4e5
RaynelX <167647066+RaynelX@users.noreply.github.com>
2026-02-22 23:57:06 +0300

    UI fix

--- src/app/layouts/MainLayout.tsx
+++ src/app/layouts/MainLayout.tsx
@@ -17,7 +17,7 @@ export function MainLayout() {
   const sw = useSwUpdate();
 
   return (
-    <div className="flex flex-col h-full fixed inset-0 bg-gray-100 dark:bg-black" style={{ height: '100dvh' }}>
+    <div className="flex flex-col h-full fixed inset-0 bg-gray-100 dark:bg-black"/* style={{ height: '100dvh' }}*/>
       {/* Header */}
       <header
         className="shrink-0 flex items-center justify-between px-4 py-3 bg-white dark:bg-neutral-900 border-b border-gray-200 dark:border-neutral-800"
@@ -46,9 +46,8 @@ export function MainLayout() {
 
       {/* Bottom Navigation */}
       <nav
-        /*className="shrink-0 flex border-t border-gray-200 dark:border-neutral-800 bg-white dark:bg-neutral-900"
-        style={{ paddingBottom: 'env(safe-area-inset-bottom, 0px)' }}*/
-        className="fixed bottom-0 left-0 right-0 z-50 bg-white dark:bg-neutral-900 backdrop-blur-md border-t border-gray-200 dark:border-neutral-800 pb-[env(safe-area-inset-bottom)]"
+        className="shrink-0 flex border-t border-gray-200 dark:border-neutral-800 bg-white dark:bg-neutral-900"
+        style={{ paddingBottom: 'env(safe-area-inset-bottom, 0px)' }}
       >
         {navItems.map(({ to, icon: Icon, label }) => (
           <NavLink
